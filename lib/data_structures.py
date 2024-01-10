0dec583e24a86b7dbc113b7ca55c07f95253b258
DianJeruto <dian.jeruto@student.moringaschool.com>
2024-01-10 15:35:48 +0300

    completed assignment

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -1,38 +1,14 @@
-spicy_foods = [
-    {
-        "name": "Green Curry",
-        "cuisine": "Thai",
-        "heat_level": 9,
-    },
-    {
-        "name": "Buffalo Wings",
-        "cuisine": "American",
-        "heat_level": 3,
-    },
-    {
-        "name": "Mapo Tofu",
-        "cuisine": "Sichuan",
-        "heat_level": 6,
-    },
-]
+# lib/data_structures.py
 
 def get_names(spicy_foods):
-    pass
+    """
+    Returns a list of names of all spicy foods.
+    """
+    return [food["name"] for food in spicy_foods]
 
-def get_spiciest_foods(spicy_foods):
-    pass
-
-def print_spicy_foods(spicy_foods):
-    pass
-
-def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
-
-def print_spiciest_foods(spicy_foods):
-    pass
-
-def get_average_heat_level(spicy_foods):
-    pass
-
-def create_spicy_food(spicy_foods, spicy_food):
-    pass
+def get_spiciest_food_names(spicy_foods):
+    """
+    Returns a list of names of the spiciest foods.
+    """
+    max_heat_level = max(food["heat_level"] for food in spicy_foods)
+    return [food["name"] for food in spicy_foods if food["heat_level"] == max_heat_level]
